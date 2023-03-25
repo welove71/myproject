57b51c57c7d301f5a67d195abb1707c88e28f4cd
welove71 <welove71@gmail.com>
2023-03-25 22:25:00 +0900

    second commit

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from myapp import views
+
+# http://127.0.0.1
+# http://127.0.0.1/app
+# http://127.0.0.1/create
+# http://127.0.0.1/read/1
+
+
+urlpatterns = [
+    path('', views.index),
+    path('create', views.create),
+    path('read/<id>/', views.read),
+]
