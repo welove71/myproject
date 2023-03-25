e1ef41b5f74b72b54047bfa444f2b60e207c7aca
welove71 <welove71@gmail.com>
2023-03-25 23:41:31 +0900

    3번째 커및

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,49 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+topics = [
+{'id':1, 'title':'routing', 'body':'Routing is...'},
+{'id':2, 'title':'view', 'body':'View is...'},
+{'id':3, 'title':'model', 'body':'Model is...'},
+]
+
+def HTMLTemplate(articelTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}"> {topic["title"]}</a> </li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href ="/">Django</a></h1>
+        <ol>
+            {ol}
+        </ol>
+        {articelTag}
+    </body>
+    </html>
+
+    '''
+
 def index(request):
-    return HttpResponse('Welcome!')
+    article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+
+def read(request, id):
+    global topics
+    article =''
+    for topic in topics:
+        if topic['id'] == int(id):
+           article = f'<h2>{topic["title"]}</h2>{topic["body"]}' 
+
+    return HttpResponse(HTMLTemplate(article))
+
 
 def create(request):
     return HttpResponse('Create')
 
-def read(request, id):
-    return HttpResponse('Read!'+id)
+
